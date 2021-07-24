071516997775ff4a1a1874067e99d1664edea1eb
Osazee Isere <oz1.concepts@gmail.com>
2021-07-24 03:38:41 +0100

    initial commit

--- LamdaExpressionsMultiplesParam/Program.cs
+++ LamdaExpressionsMultiplesParam/Program.cs
@@ -10,6 +10,25 @@ namespace LamdaExpressionsMultiplesParam
     {
         static void Main(string[] args)
         {
+
+            // Register with delegate as Lamda Expression
+            SimpleMath m = new SimpleMath();
+            m.SetMathHandler((mesg, result) =>
+            {
+                Console.WriteLine("{0}, result: {1}",mesg,result);
+            });
+
+            //the following execute the Lamda Expression
+            m.Add(10, 10);
+
+            //trying to print "Enjoy your string"
+            VerySimpleDelegate d = new VerySimpleDelegate(() => { return "Enjoy you String"; });
+            Console.WriteLine(d());
+
+            Console.ReadLine();
+
         }
+        public delegate string VerySimpleDelegate();
+
     }
 }
